8ee6a1d98cc067f4033e33adafd6ae1fab470700
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-09 00:55:54 +0700

    add swagger to document API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
 import { Config } from './shared/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Gomoku online')
+    .setDescription('The Gomoku online API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addOAuth2()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('document', app, document);
+
   await app.listen(Config.getCurrentHost().port);
 }
 bootstrap();
