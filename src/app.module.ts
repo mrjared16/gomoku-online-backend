22b5829fa1d7024ef06be10f8296793c95d7f2ba
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-08 19:02:32 +0700

    config TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+import { Config } from './shared/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(Config.getTypeORMConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(connection: Connection) { }
+}
