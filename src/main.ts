51210a7d45453f1bda62b8fda876de838cc32e8b
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-08 19:26:43 +0700

    config port of web server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Config } from './shared/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(Config.getCurrentHost().port);
 }
 bootstrap();
