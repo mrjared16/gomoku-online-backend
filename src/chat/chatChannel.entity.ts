7923b46fc56d30929a4175f043c3519094244e00
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-15 16:44:11 +0700

    add chat relate entity

---
+++ src/chat/chatChannel.entity.ts
@@ -0,0 +1,25 @@
+import { GameEntity } from './../game/game.entity';
+import { UserEntity } from 'src/users/users.entity';
+import { CreateDateColumn, Entity, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ChatRecordEntity } from './chatRecord.entity';
+
+@Entity('chat_channel')
+export class ChatChannelEntity {
+  @ManyToMany(() => UserEntity, user => user.chats)
+  @JoinTable({
+    name: 'chat_participant'
+  })
+  users: UserEntity[];
+
+  @OneToMany(() => ChatRecordEntity, record => record.channel)
+  records: ChatRecordEntity[];
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToOne(() => GameEntity, game => game.chat)
+  game: GameEntity;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+}
---
+++ src/chat/chatRecord.entity.ts
@@ -0,0 +1,21 @@
+import { UserEntity } from 'src/users/users.entity';
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ChatChannelEntity } from './chatChannel.entity';
+
+@Entity('chat_record')
+export class ChatRecordEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => UserEntity, user => user.chatRecords)
+  user: UserEntity;
+
+  @ManyToOne(() => ChatChannelEntity, channel => channel.records)
+  channel: ChatChannelEntity;
+
+  @Column({ nullable: false })
+  content: string;
+
+  @CreateDateColumn({ type: "timestamptz", default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+}
