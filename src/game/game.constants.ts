1f6073c2b7376e12fc9ef12ed8a3c6e1bd66d9a4
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-21 19:32:44 +0700

    add board size to entity

---
+++ src/game/game.constants.ts
@@ -0,0 +1,2 @@
+const DEFAULT_BOARD_SIZE = 20;
+export { DEFAULT_BOARD_SIZE };
--- src/game/game.entity.ts
+++ src/game/game.entity.ts
@@ -1,4 +1,10 @@
-import { Column, CreateDateColumn, JoinColumn, ManyToMany, OneToOne } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToMany,
+  OneToOne,
+} from 'typeorm';
 import { UserEntity } from 'src/users/users.entity';
 import { OneToMany } from 'typeorm';
 import { ChatChannelEntity } from '../chat/chatChannel.entity';
@@ -6,31 +12,38 @@ import { PrimaryGeneratedColumn } from 'typeorm';
 import { Entity } from 'typeorm';
 import { RankRecordEntity } from 'src/gameHistory/rankRecord.entity';
 import { GameSide, MoveRecordEntity } from 'src/gameHistory/moveRecord.entity';
+import { DEFAULT_BOARD_SIZE } from './game.constants';
 @Entity('game')
-export class GameEntity{ 
+export class GameEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToMany(() => UserEntity, user => user.games)
+  @ManyToMany(() => UserEntity, (user) => user.games)
   users: UserEntity[];
 
-  @OneToMany(() => MoveRecordEntity, move => move.game) 
+  @OneToMany(() => MoveRecordEntity, (move) => move.game)
   moves: MoveRecordEntity[];
-  
-  @OneToMany(() => RankRecordEntity, record => record.game)
+
+  @OneToMany(() => RankRecordEntity, (record) => record.game)
   rankRecords: RankRecordEntity[];
 
-  @OneToOne(() => ChatChannelEntity, chat => chat.game)
+  @OneToOne(() => ChatChannelEntity, (chat) => chat.game)
   @JoinColumn()
   chat: ChatChannelEntity;
 
   @Column({
     type: 'enum',
     enum: GameSide,
-    nullable: false
+    nullable: false,
   })
   winSide: GameSide;
-  
+
+  @Column({
+    type: 'int',
+    default: DEFAULT_BOARD_SIZE,
+  })
+  boardSize: number;
+
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   start_at: Date;
 
