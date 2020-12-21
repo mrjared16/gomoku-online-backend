a81b5e5826eada3c81218849ff28164637968074
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-21 20:21:15 +0700

    create game history interface

---
+++ src/gameHistory/gameHistory.dto.ts
@@ -0,0 +1,36 @@
+import { RankRecordEntity } from 'src/gameHistory/rankRecord.entity';
+import { MoveRecordEntity } from 'src/gameHistory/moveRecord.entity';
+import { GameSide } from './moveRecord.entity';
+
+export class MoveRecordDTO {
+  id: string;
+  position: number;
+  value: GameSide;
+  time: Date;
+
+  static EntityToDTO(moveEntity: MoveRecordEntity): MoveRecordDTO {
+    if (moveEntity == null) return null;
+
+    const { id, value, position, created_at } = moveEntity;
+    return { id, position, value, time: created_at };
+  }
+}
+
+export class RankRecordDTO {
+  gameID: string;
+  playerID: string;
+  oldRank: number;
+  newRank: number;
+
+  static EntityToDTO(rankEntity: RankRecordEntity): RankRecordDTO {
+    if (rankEntity == null) return null;
+
+    const { user, game, oldRank, newRank } = rankEntity;
+    return {
+      playerID: user.id,
+      gameID: game.id,
+      oldRank,
+      newRank,
+    };
+  }
+}
