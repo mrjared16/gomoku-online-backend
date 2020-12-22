62dd491547525fa70e8f0a02397b9b83bbd87fc2
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-22 15:07:59 +0700

    refactor room model (add option and users)

--- src/room/room.model.ts
+++ src/room/room.model.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Client } from 'socket.io/dist/client';
 import { Socket } from 'socket.io/dist/socket';
+import { GomokuGamePlayer } from 'src/game/game.dto';
+import { GameEntity } from 'src/game/game.entity';
 import { GameModel } from 'src/game/game.model';
+import { GameSide } from 'src/gameHistory/moveRecord.entity';
 import { UserDTO } from 'src/users/users.dto';
+import { GameService } from './../game/game.service';
+import { DEFAULT_ROOM_OPTION } from './room.constants';
+import { RoomOption, RoomRequirement } from './room.dto';
 
 @Injectable()
 export class RoomManager {
   constructor() {
     console.log('room Manager is initialize');
   }
+
   map: Map<string, RoomModel> = new Map();
+
   getRoom = (roomID): RoomModel => {
     return this.map.get(roomID);
   };
@@ -19,15 +26,13 @@ export class RoomManager {
       return Date.now().toString(36) + Math.random().toString(36).substr(2);
     };
     const roomID = getRoomID();
-    console.log({ hostSocket });
-    const newRoom = new RoomModel(roomID, host, null);
+    const newRoom = new RoomModel(roomID, host);
     this.map.set(roomID, newRoom);
     return newRoom;
   }
+
   getRooms(): RoomModel[] {
     return Array.from(this.map.values());
-    // .filter(([key, values]) => key != '')
-    // .map(([key, value]) => value);
   }
 }
 
@@ -35,32 +40,71 @@ export class RoomModel {
   constructor(
     public id: string,
     public host: UserDTO,
-    public hostSocket: Client,
-    public opponent: UserDTO | null = null,
-    public boardSize: 20 = 20,
+    public roomOption: RoomOption = DEFAULT_ROOM_OPTION,
   ) {
-    this.game = new GameModel(this.boardSize);
-  }
-  public game: GameModel;
+    const { boardSize, password, time } = roomOption;
+    this.time = time;
+    this.boardSize = boardSize;
+    this.password = password;
 
-  addUser(user: UserDTO): boolean {
-    if (!user || !user.id) return false;
-    if (
-      this.opponent &&
-      user.id != this.host.id &&
-      user.id != this.opponent.id
-    ) {
-      return false;
+    this.players = {
+      X: null,
+      O: null,
+    };
+    this.gameID = null;
+  }
+
+  private password: string;
+  private boardSize: number;
+  private time: number;
+
+  public players: GomokuGamePlayer;
+  users: UserDTO[];
+
+  public gameID: string;
+  gameModel: GameModel;
+
+  addUser(user: UserDTO, roomRequirement: RoomRequirement): boolean {
+    if (roomRequirement) {
+      const { password } = roomRequirement;
+      if (this.password !== password) return false;
     }
-    if (user.id != this.host.id) {
-      this.opponent = user;
+    const isExist = this.users.some((inRoomUser) => inRoomUser.id === user.id);
+    if (!isExist) {
+      this.users.push(user);
     }
     return true;
   }
-  hit(index: number, value: 0 | 1) {
-    this.game.hit(index, value);
+
+  setPlayer(user: UserDTO, gameSide: GameSide): boolean {
+    //TODO: refactor gameside
+    if (this.players[gameSide.toString()]) return false;
+    this.players[gameSide.toString()] = user;
+    return true;
   }
-  getTurn(): string {
-    return this.game.getTurn() == 0 ? this.host.id : this.opponent.id;
+
+  kickPlayer(user: UserDTO): boolean {
+    // TODO: refactor
+    const gameSide = [GameSide.O.toString(), GameSide.X.toString()].find(
+      (team: 'X' | 'O') => this.players[team].id === user.id,
+    );
+    if (!gameSide) return false;
+    this.players[gameSide] = null;
+    return true;
+  }
+
+  startGame(gameService: GameService) {
+    this.gameModel = gameService.createGame(this);
+    this.gameID = this.gameModel.getGameID();
+    return this.gameID;
+  }
+
+  endGame() {
+    this.gameModel = null;
+    this.gameID = null;
+  }
+
+  save() {
+    //TODO: handle save game
   }
 }
