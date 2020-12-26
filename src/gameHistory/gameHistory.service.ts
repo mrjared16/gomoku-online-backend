6b8421e0e6bbd610410dd3abd615a657fa5c5948
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-26 18:24:29 +0700

    save move history to game

--- src/gameHistory/gameHistory.service.ts
+++ src/gameHistory/gameHistory.service.ts
@@ -1,9 +1,12 @@
+import { MoveRecordEntity } from './moveRecord.entity';
+import { RankRecordEntity } from 'src/gameHistory/rankRecord.entity';
 import { UserEntity } from 'src/users/users.entity';
 import { GameSide, GomokuGamePlayer } from 'src/game/game.dto';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { TeamEntity } from './team.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { RoomModel } from 'src/room/room.model';
 @Injectable()
 export class GameHistoryService {
   constructor(
@@ -11,6 +14,8 @@ export class GameHistoryService {
     private teamRepository: Repository<TeamEntity>,
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
+    @InjectRepository(MoveRecordEntity)
+    private moveRecordRepository: Repository<MoveRecordEntity>,
   ) {}
 
   async createTeamEntity(players: GomokuGamePlayer): Promise<TeamEntity[]> {
@@ -27,7 +32,29 @@ export class GameHistoryService {
         side: GameSide.O,
       }),
     ];
-    await this.teamRepository.save(result);
     return result;
+  }
+
+  async saveGame(room: RoomModel) {
+    // TODO: save chat
+    // save moves
+    room.getGame().getGameEntity().moves = room
+      .getGame()
+      .getMoves()
+      .map((moveDTO) => {
+        const { id, position, value, time } = moveDTO;
+        const playerID = room.getPlayerOfSide(value);
+        const player = this.userRepository.create({ id: playerID });
+        return this.moveRecordRepository.create({
+          user: player,
+          position,
+          value,
+          created_at: time,
+        });
+      });
+
+    // TODO: save rank records
+    const rankRecord: RankRecordEntity[] = [];
+    room.getGame().getGameEntity().rankRecords = rankRecord;
   }
 }
