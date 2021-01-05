e34b2696d5d8005c8b6e84e914e8494af0c7f3b9
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-05 14:16:34 +0700

    save new rank and change matches info of user

--- src/gameHistory/gameHistory.service.ts
+++ src/gameHistory/gameHistory.service.ts
@@ -1,12 +1,14 @@
-import { MoveRecordEntity } from './moveRecord.entity';
-import { RankRecordEntity } from 'src/gameHistory/rankRecord.entity';
-import { UserEntity } from 'src/users/users.entity';
-import { GameSide, GomokuGamePlayer } from 'src/game/game.dto';
-import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
-import { TeamEntity } from './team.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { GameSide, GomokuGamePlayer } from 'src/game/game.dto';
+import { RankRecordEntity } from 'src/gameHistory/rankRecord.entity';
 import { RoomModel } from 'src/room/room.model';
+import { UserEntity } from 'src/users/users.entity';
+import { Repository } from 'typeorm';
+import { UserService } from './../users/users.service';
+import { getNewRank, GetRankData, getUserResult } from './gameHistory.helper';
+import { MoveRecordEntity } from './moveRecord.entity';
+import { TeamEntity } from './team.entity';
 @Injectable()
 export class GameHistoryService {
   constructor(
@@ -16,6 +18,8 @@ export class GameHistoryService {
     private userRepository: Repository<UserEntity>,
     @InjectRepository(MoveRecordEntity)
     private moveRecordRepository: Repository<MoveRecordEntity>,
+    @InjectRepository(RankRecordEntity)
+    private rankRecordRepository: Repository<RankRecordEntity>,
   ) {}
 
   async createTeamEntity(players: GomokuGamePlayer): Promise<TeamEntity[]> {
@@ -37,6 +41,7 @@ export class GameHistoryService {
 
   async saveGame(room: RoomModel) {
     // TODO: save chat
+
     // save moves
     room.getGame().getGameEntity().moves = room
       .getGame()
@@ -54,7 +59,45 @@ export class GameHistoryService {
       });
 
     // TODO: save rank records
-    const rankRecord: RankRecordEntity[] = [];
+    const gameResult = room.getGame().getGameResult();
+    const team = room.getGame().getGameEntity().team;
+
+    const avgRankOf2Team = new GetRankData(team);
+
+    const rankRecord: RankRecordEntity[] = team.reduce(
+      (currentRankRecord, currentTeam) => {
+        const { users, side } = currentTeam;
+
+        const currentTeamRankRecord = users.map((currentUser) => {
+          const { isDraw, isWon } = getUserResult(side, gameResult);
+
+          const oldRank = currentUser.rank;
+          const newRank = getNewRank({
+            currentRank: oldRank,
+            enemyRank: avgRankOf2Team.getAvgRankOfEnemy(side),
+            isDraw,
+            isWon,
+          });
+
+          currentUser.rank = newRank;
+          currentUser.numberOfMatches = currentUser.numberOfMatches + 1;
+          if (isWon) {
+            currentUser.numberOfWonMatches = currentUser.numberOfWonMatches + 1;
+          }
+
+          return this.rankRecordRepository.create({
+            user: currentUser,
+            oldRank: oldRank,
+            newRank: newRank,
+          });
+        });
+
+        currentRankRecord.push(...currentTeamRankRecord);
+        return currentRankRecord;
+      },
+      [] as RankRecordEntity[],
+    );
+
     room.getGame().getGameEntity().rankRecords = rankRecord;
   }
 }
