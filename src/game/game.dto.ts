17cb5b68d973bbc637d681c857088ab787614362
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-09 15:10:16 +0700

    add game DTO

--- src/game/game.dto.ts
+++ src/game/game.dto.ts
@@ -1,5 +1,32 @@
+import { GameEntity } from 'src/game/game.entity';
+import { ChatRecordDTO } from 'src/chat/chat.dto';
 import { MoveRecordDTO } from 'src/gameHistory/gameHistory.dto';
 import { UserDTO } from 'src/users/users.dto';
+
+export class GameDTO {
+  id: string;
+  boardSize: number;
+  result: GameResult;
+  startAt: Date;
+  duration: number;
+
+  moveRecord: MoveRecordDTO[];
+  chatRecord: ChatRecordDTO[];
+  static EntityToDTO(gameEntity: GameEntity): GameDTO {
+    const { id, boardSize, gameResult, start_at, duration } = gameEntity;
+    const moveRecord: MoveRecordDTO[] = [];
+    const chatRecord: ChatRecordDTO[] = [];
+    return {
+      id,
+      boardSize,
+      result: gameResult,
+      startAt: start_at,
+      duration,
+      moveRecord: moveRecord,
+      chatRecord: chatRecord,
+    };
+  }
+}
 
 export class GameState {
   move: MoveRecordDTO[];
