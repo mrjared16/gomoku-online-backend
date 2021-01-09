a20f135cd1d5f77ea1ca86eb2905f3bd3cc58901
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-10 00:32:12 +0700

    handle chat and create chat record

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,12 +1,19 @@
+import { AuthModule } from 'src/auth/auth.module';
+import { RoomModule } from './../room/room.module';
 import { ChatService } from './chat.service';
 import { ChatChannelEntity } from 'src/chat/chatChannel.entity';
 import { ChatRecordEntity } from 'src/chat/chatRecord.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { UserEntity } from 'src/users/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatRecordEntity, ChatChannelEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ChatRecordEntity, ChatChannelEntity, UserEntity]),
+    RoomModule,
+    AuthModule,
+  ],
   exports: [ChatService, ChatGateway],
   providers: [ChatService, ChatGateway],
 })
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,9 +1,14 @@
-import { RoomModel } from 'src/room/room.model';
-import { ChatRecordEntity } from 'src/chat/chatRecord.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Socket } from 'socket.io';
 import { ChatChannelEntity } from 'src/chat/chatChannel.entity';
+import { ChatRecordEntity } from 'src/chat/chatRecord.entity';
+import { RoomManager } from 'src/room/room.model';
 import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import { ChatRecordDTO, SentMessageToRoomChatDTO } from './chat.dto';
+import { ChatGateway } from './chat.gateway';
+import { UserEntity } from 'src/users/users.entity';
 @Injectable()
 export class ChatService {
   constructor(
@@ -11,10 +16,52 @@ export class ChatService {
     private chatRepository: Repository<ChatChannelEntity>,
     @InjectRepository(ChatRecordEntity)
     private chatRecordRepository: Repository<ChatRecordEntity>,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    @Inject(forwardRef(() => ChatGateway))
+    private chatGateway: ChatGateway,
+    private roomManager: RoomManager,
+    @Inject(forwardRef(() => AuthService))
+    private authService: AuthService,
   ) {}
 
   async createChatChannelForRoom(): Promise<ChatChannelEntity> {
     const newChatChannel = this.chatRepository.create();
     return await this.chatRepository.save(newChatChannel);
+  }
+
+  async handleSentMessageToRoomChat(
+    socket: Socket,
+    roomMessageData: SentMessageToRoomChatDTO,
+  ) {
+    const { roomID, data } = roomMessageData;
+    const { token } = data;
+
+    const room = this.roomManager.getRoom(roomID);
+    if (!room) {
+      return;
+    }
+
+    const userInfo = await this.authService.getUser(token);
+    if (!userInfo) {
+      return;
+    }
+
+    const chatChannel = room.getChatChannelEntity();
+    const { content } = data;
+    const newChatRecord = this.chatRecordRepository.create({
+      channel: chatChannel,
+      user: this.userRepository.create({ id: userInfo.id }),
+      content: content,
+    });
+
+    const newChatRecordResponse = await this.chatRecordRepository.save(
+      newChatRecord,
+    );
+
+    this.chatGateway.broadcastChatEventToMember(socket, roomID, {
+      event: 'onReceivedMessage',
+      data: ChatRecordDTO.EntityToDTO(newChatRecordResponse),
+    });
   }
 }
