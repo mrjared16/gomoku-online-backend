b1c893e2a027c373d44edc09631423346e5d6c96
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-09 18:33:31 +0700

    add all game history response

--- src/gameHistory/gameHistory.interface.ts
+++ src/gameHistory/gameHistory.interface.ts
@@ -1,6 +1,15 @@
-import { ApiResponseProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiResponseProperty } from '@nestjs/swagger';
 import { GameDTO } from './../game/game.dto';
+import { GameHistoryDTO } from './gameHistory.dto';
 export class GameHistoryDetailResponse {
   @ApiResponseProperty()
   game: GameDTO;
+}
+
+export class AllGameHistoryResponse {
+  @ApiProperty({
+    type: GameHistoryDTO,
+    isArray: true,
+  })
+  games: GameHistoryDTO[];
 }
